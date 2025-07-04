2f5aad6437a7764162bb83d853893d393ad0945f
Jashneer <jashneerkaur9@gmail.com>
2025-07-04 12:32:00 +0530

    Swap using third variable

---
+++ swap.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+
+    cin >> a;
+    cin >> b;
+
+    int temp = a;
+    a = b;
+    b = temp;
+
+    cout << a << endl;
+    cout <<b;
+}

dd737d00ceadf5eff4b82a23be7fc10e4dea0a68
Jashneer <jashneerkaur9@gmail.com>
2025-07-11 12:10:47 +0530

    Program to remove duplicate elements in an array

---
+++ removeduplicate.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter " << n << " elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int newSize = n;
+    for(int i = 0; i < newSize; i++) {
+        for(int j = i + 1; j < newSize; ) {
+            if(arr[i] == arr[j]) {
+                // Shift elements left
+                for(int k = j; k < newSize - 1; k++) {
+                    arr[k] = arr[k + 1];
+                }
+                newSize--;
+            } else {
+                j++;
+            }
+        }
+    }
+
+    cout << "Array after removing duplicates: ";
+    for(int i = 0; i < newSize; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
